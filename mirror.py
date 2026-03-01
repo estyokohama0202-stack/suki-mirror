e683026ff88fc470f395265052af8b8f9a0eea13
estyokohama0202-stack <estyokohama0202@gmail.com>
2026-03-01 12:06:11 +0900

    Update mirror.py

--- mirror.py
+++ mirror.py
@@ -4,7 +4,6 @@ import requests
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -22,10 +21,9 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1920,1080")
-options.binary_location = "/usr/bin/chromium"
 
-service = Service("/usr/bin/chromedriver")
-driver = webdriver.Chrome(service=service, options=options)
+# 🔥 ここ重要：パス指定しない
+driver = webdriver.Chrome(options=options)
 
 while True:
     try:
